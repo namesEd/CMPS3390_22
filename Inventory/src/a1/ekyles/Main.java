f005d4b1e0b637a1b63a1ec0d0b7dfc5eb3e3f0c
namesEd <edkyles08@gmail.com>
2022-02-11 16:36:49 -0800

    Started A2

--- Inventory/src/a1/ekyles/Main.java
+++ Inventory/src/a1/ekyles/Main.java
@@ -6,48 +6,26 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    private static final Random ran = new Random();
+    private static final FoodItems[] foodItems = FoodItems.values();
+    private static final Tools[] tools = Tools.values();
+    private static final ToolUses[] toolUses = ToolUses.values();
+    private static final Potions[] potions = Potions.values();
+    private static final PotionUses[] potionUses = PotionUses.values();
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Random ran = new Random();
         List<Item> items = new ArrayList<>();
-        FoodItems[] foodItems = FoodItems.values();
-        Tools[] tools = Tools.values();
-        ToolUses[] toolUses = ToolUses.values();
-        Potions[] potions = Potions.values();
-        PotionUses[] potionUses = PotionUses.values();
-
+        Scanner scan = new Scanner(System.in);
+        //Srt of the app
         System.out.print("How many items do you want: ");
         int itemCnt = Integer.parseInt(scan.nextLine());
 
         for(int i = 0; i < itemCnt; i++) {
             int type = ran.nextInt(3);
             switch(type) {
-                case 0:
-                    int foodIndex = ran.nextInt(foodItems.length);
-                    String foodName = foodItems[foodIndex].toString();
-                    float foodPrice = ran.nextFloat(10);
-                    int foodQty = ran.nextInt(35);
-                    int foodUses = ran.nextInt(7);
-                    float healthGain = ran.nextFloat(25);
-                    Food tmpFood = new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
-                    items.add(tmpFood);
-                case 1:
-                    int toolIndex = ran.nextInt(tools.length);
-                    String toolName = tools[toolIndex].toString();
-                    float toolPrice = ran.nextFloat(200);
-                    int toolQty = ran.nextInt(15);
-                    String use = toolUses[toolIndex].toString();
-                    Tool tmpTool = new Tool(toolName, toolPrice, toolQty, use);
-                    items.add(tmpTool);
-                case 2:
-                    int potionIndex = ran.nextInt(potions.length);
-                    String potionName = potions[potionIndex].toString();
-                    float potionPrice = ran.nextFloat(125);
-                    int potionQty = ran.nextInt(5);
-                    String uses = potionUses[potionIndex].toString();
-                    Potion tmpPotion = new Potion(potionName, potionPrice, potionQty, uses);
-                    items.add(tmpPotion);
+                case 0 -> items.add(genFood());
+                case 1 -> items.add(genTool());
+                case 2 -> items.add(genPotion());
             }
         }
 
@@ -55,5 +33,32 @@ public class Main {
             System.out.println(i);
         }
 
+    }
+    public static Food genFood() {
+        int foodIndex = ran.nextInt(foodItems.length);
+        String foodName = foodItems[foodIndex].toString();
+        float foodPrice = ran.nextFloat(10);
+        int foodQty = ran.nextInt(35);
+        int foodUses = ran.nextInt(7);
+        float healthGain = ran.nextFloat(25);
+        return new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
+    }
+
+    public static Tool genTool(){
+        int toolIndex = ran.nextInt(tools.length);
+        String toolName = tools[toolIndex].toString();
+        float toolPrice = ran.nextFloat(200);
+        int toolQty = ran.nextInt(15);
+        String use = toolUses[toolIndex].toString();
+        return new Tool(toolName, toolPrice, toolQty, use);
+    }
+
+    public static Potion genPotion(){
+        int potionIndex = ran.nextInt(potions.length);
+        String potionName = potions[potionIndex].toString();
+        float potionPrice = ran.nextFloat(125);
+        int potionQty = ran.nextInt(5);
+        String uses = potionUses[potionIndex].toString();
+        return new Potion(potionName, potionPrice, potionQty, uses);
     }
 }
---
+++ MultiThreadSort/src/a2/ekyles/Main.java
@@ -0,0 +1,11 @@
+package a2.ekyles;
+import a1.ekyles.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Food f = a1.ekyles.Main.genFood();
+        System.out.println(f);
+    }
+}
